845d38b2e98368bab17cb38d3189e4b71353259d
Noah Lundquist <nalundquist@gmail.com>
2022-08-22 13:15:28 -0700

    WIP: adding UI functionality

---
+++ README.md
@@ -0,0 +1,8 @@
+description: destination();
+
+test: anything under the prototype destination has traits: location, landmarks, best season, notes
+
+code: let sea = destination("Seattle", "Space Needle", "Summer", "Passive Aggressive lol")
+console.log(sea);
+
+Expected output: "Location: "Seattle", Landmarks: "Space Needle", Best Season: "Summer", Other Notes: "Passive Aggressive lol"
--- js/scripts.js
+++ js/scripts.js
@@ -16,4 +16,64 @@ let nola = new Destination("New Orleans", "French Quarter", "Fall/Winter", "Fren
 
  // User Interface Logic ---------------
 
- 
+function formSubmitSea(event){
+	event.preventDefault();
+
+	// document.querySelector("h2#landmark").innerText = null;
+	// document.querySelector("h2#season").innerText = null;
+	// document.querySelector("h2#notes").innerText = null;
+
+	document.querySelector("h2#landmark").innerText = "Landmarks: " + sea[1];
+	document.querySelector("h2#season").innerText = "Best Season to Visit: " + sea[2];
+	document.querySelector("h2#notes").innerText = "Other Notes: " + sea[3];
+	console.log(sea[3])
+}
+
+function formSubmitSf(event){
+	event.preventDefault();
+document.querySelector("h2#landmark").innerText = "Landmarks: " + sf[1];	
+document.querySelector("h2#season").innerText = "Best Season to Visit: " + sf[2];
+document.querySelector("h2#notes").innerText = "Other Notes: " + sf[3]
+console.log(sf[3])
+}
+
+function formSubmitNyc(event){
+	event.preventDefault();
+document.querySelector("h2#landmark").innerText = "Landmarks: " + nyc[1];
+document.querySelector("h2#season").innerText = "Best Season to Visit: " + nyc[2];
+document.querySelector("h2#notes").innerText = "Other Notes: " + nyc[3];
+	console.log(nyc[3])
+}
+
+function formSubmitPort(event){
+	event.preventDefault();
+document.querySelector("h2#landmark").innerText = "Landmarks: " + port[1];
+document.querySelector("h2#season").innerText = "Best Season to Visit: " + port[2];
+document.querySelector("h2#notes").innerText = "Other Notes: " + port[3];
+console.log(port[3])
+}
+
+function formSubmitNola(event){
+	event.preventDefault();
+document.querySelector("h2#landmark").innerText = "Landmarks: " + nola[1];
+document.querySelector("h2#season").innerText = "Best Season to Visit: " + nola[2];
+document.querySelector("h2#notes").innerText = "Other Notes: " + nola[3];
+console.log(nola[3])
+}
+
+
+window.addEventListener("load", function(){
+	let form = document.querySelector("form#destForm")
+	console.log("window load event listener")
+	form.addEventListener("submit", formSubmit);
+	let seaForm = document.querySelector("button#seattle")
+	seaForm.addEventListener("submit", function(){})
+	let sfForm = document.querySelector("button#sanFran")
+	sfForm.addEventListener("submit", function(){})
+	let nycForm = document.querySelector("button#newYork")
+	nycForm.addEventListener("submit", function(){})
+	let pdxForm = document.getElementById("pdxForm")
+	pdxForm.addEventListener("submit", function(){})
+	let nolaForm = document.querySelector("button#newOrleans")
+	nolaForm.addEventListener("submit", function(){})
+})
